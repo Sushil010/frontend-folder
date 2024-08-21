64182104148e09a7559947672acc04f5fd22607f
sushil sharma <sharma.s420823@gmail.com>
2024-08-21 08:21:10 +0545

    -

--- cloned-sites/cyberfiction/script.js
+++ cloned-sites/cyberfiction/script.js
@@ -421,3 +421,47 @@ function canvasimg(){
   }
   
   canvasimg();
+
+
+
+
+
+gsap.to("#page1",{
+  scrollTrigger:{
+    trigger:"#page1",
+    start:"top top",
+    end:"bottom top",
+    scroller:"#main",
+    pin:true
+  }
+})  
+
+gsap.to("#page2",{
+  scrollTrigger:{
+    trigger:"#page2",
+    start:"top top",
+    end:"bottom top",
+    scroller:"#main",
+    pin:true
+  }
+})  
+
+gsap.to("#page3",{
+  scrollTrigger:{
+    trigger:"#page3",
+    start:"top top",
+    end:"bottom top",
+    scroller:"#main",
+    pin:true
+  }
+})  
+
+gsap.to("#page4",{
+  scrollTrigger:{
+    trigger:"#page4",
+    start:"top top",
+    end:"bottom top",
+    scroller:"#main",
+    pin:true
+  }
+})  
